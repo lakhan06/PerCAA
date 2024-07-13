7876a7c4f5f4726740692d955e1a40ff5e5ec2e9
Lakhan Gupta <2021pcecslakhan090@poornima.org>
2024-07-13 15:53:03 +0530

    topimage part added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import './App.css'
 import { Routes , Route , useNavigate } from 'react-router-dom'
 import ProgramCommitee from './components/ProgramCommitee/ProgramCommitee'
 import SteeringCommittee from './components/SteeringCommitee/SteeringCommitee'
+import Crousal from './components/topimagesSection/Crousal'
+
 function App() {
   return (
     <>
       <div>
+    <Crousal></Crousal>
         <Routes>
           <Route path='/program-commitee' element={<ProgramCommitee/>}></Route>
           <Route path='/steering-commitee' element={<SteeringCommittee/>}></Route>
