905767d0c7c3041d8c2a7b81121fff0ca5d2f0df
Lakhan Gupta <2021pcecslakhan090@poornima.org>
2024-07-13 18:21:16 +0530

    contact page updated

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -18,11 +18,11 @@ const Contact = () => {
                                 <div className="contactDesignation">Program Chair</div>
                             </div>
                             <div className="contactCardRight">
-                                <p className='contact_CI1'>Contact Information</p>
-                                <p className='contact_mailphone'><MdEmail /> <a href="mailto:nikita.jain@poornima.org">nikita.jain@poornima.org</a></p>
-                                <p className='contact_mailphone'><FaPhoneAlt /> <a href="tel:+919413069023">+91 9413069023</a></p>
-                                <p><span>Address:</span> ISI-6, RIICO Institutional Area, Sitapura, Jaipur-302022</p>
-                                <p><span>Conference Email:</span> <a href="mailto:conference@example.com">conference@example.com</a></p>
+                                {/* <p className='contact_CI1 contactborder'>Contact Information</p> */}
+                                <p className='contact_mailphone contactborder'><MdEmail /> <a href="mailto:nikita.jain@poornima.org">nikita.jain@poornima.org</a></p>
+                                <p className='contact_mailphone contactborder'><FaPhoneAlt /> <a href="tel:+919413069023">+91 9413069023</a></p>
+                                <p className='contactborder'><span>Address:</span> ISI-6, RIICO Institutional Area, Sitapura, Jaipur-302022</p>
+                                <p className='contactborder'><span>Conference Email:</span> <a href="mailto:conference@example.com">conference@example.com</a></p>
                             </div>
                         </div>
                     </div>
