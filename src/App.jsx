48d7e7b17708bdcee523e1eadf6487c747d9328a
Abhinav Arora <abhinavarora310@gmail.com>
2024-07-13 11:47:18 +0530

    commitee-pages-added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
-
+import { Routes , Route , useNavigate } from 'react-router-dom'
+import ProgramCommitee from './components/ProgramCommitee/ProgramCommitee'
+import SteeringCommittee from './components/SteeringCommitee/SteeringCommitee'
 function App() {
   return (
     <>
       <div>
+        <Routes>
+          <Route path='/program-commitee' element={<ProgramCommitee/>}></Route>
+          <Route path='/steering-commitee' element={<SteeringCommittee/>}></Route>
+        </Routes>
       </div>
     </>
   )
--- src/components/ProgramCommitee/ProgramCommitee.jsx
+++ src/components/ProgramCommitee/ProgramCommitee.jsx
@@ -1,5 +1,31 @@
-return(
-    <div>
-        hello
-    </div>
-)
+import data from "./data.json"
+import "./ProgramCommitee.css"
+const ProgramCommitee = ()=>{    
+    return(
+        <div className="main">
+            <div className="wrapper">
+            <div className="heading">
+                Program Commitee : 
+            </div>
+            <div className="member-container">
+                {
+                    
+                    data.map((member , index)=>(
+                        <div className="member-info">
+                            <div className="member-name">
+                                {member.name}
+                            </div>
+                            <div>
+                            {member.title ? `${member.title} ` : ''}{member.institution}, {member.country}
+                            </div>
+                            <hr></hr>
+                        </div>
+                    ))
+                }
+            </div>
+            </div>
+        </div>
+    )
+}
+
+export default ProgramCommitee;
