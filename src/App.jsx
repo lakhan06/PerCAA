5c6b8045724c5b25ddc9ae32d06692ae0cdeb2cd
Abhinav Arora <abhinavarora310@gmail.com>
2024-07-13 14:40:53 +0530

    registration-fee-component-added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { Routes , Route , useNavigate } from 'react-router-dom'
 import ProgramCommitee from './components/ProgramCommitee/ProgramCommitee'
 import SteeringCommittee from './components/SteeringCommitee/SteeringCommitee'
+import Registration from './components/Registration/Registration'
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path='/program-commitee' element={<ProgramCommitee/>}></Route>
           <Route path='/steering-commitee' element={<SteeringCommittee/>}></Route>
+          <Route path='/registration' element={<Registration/>}></Route>
         </Routes>
       </div>
     </>
