d1128dcf113fc400339c8bdf3e173caa431898bb
Lakhan Gupta <2021pcecslakhan090@poornima.org>
2024-07-14 10:55:57 +0530

    change

--- src/components/topComp/TopComp.jsx
+++ src/components/topComp/TopComp.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import './topComp.css'
-import pce from '/Assets/Poornima_Logo.jpg'
+import pce from '/Assets/Poornima_Logo.png'
 import ACM from '/logos/acmChapterLogo.png'
 import MNIT from '/logos/Mnit_logo.png'
 import IEEE from '/logos/IEE_logo.jpg'
-import BL  from '/Assets/bentham_logo.webp'
+import BL  from '/Assets/bentham_logo.png'
 function TopComp() {
   return (
     <div className='topComp'>
+
+    <marquee className="topmarquee" scrollamount="20">International Conference on Pervasive Computing Advances and Applications</marquee>
         <div className='top'>
             <div className="top-logos">
                 <img src={pce} alt="poornima university logo" className='pce_logo'/>
                 <img src={ACM} alt="acm chapter logo" className='acm_logo'/>
             </div>
             <div className="top-head">
-                <div className='top-line'>International Conference on Pervasive Computing Advances and Applications</div>
-                <div className='top-line'>PERCAA - 2024</div>
+                {/* <div className='top-line'>International Conference on Pervasive Computing Advances and Applications</div> */}
+                <div className='top-line percaa'>PERCAA - 2024</div>
                 <div className='top-line'> -- Dates -- </div>
             </div>
             <div className="top-logos">
