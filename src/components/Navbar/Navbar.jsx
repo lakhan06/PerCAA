a8af1f51893b1e3e0682f181b3534aede26bf0a2
Abhinav Arora <abhinavarora310@gmail.com>
2024-07-16 15:32:08 +0530

    init

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -18,50 +18,50 @@ const Navbar = () => {
       </div>
       <nav id="mainav" className={`navbar ${toggle ? "toggleActive" : ""}`}>
         <ul className="clear">
-          <li className="active"><Link to={"/"}>Home</Link></li>
+          <li className="active"><Link onClick={handleToggle} to={"/"}>Home</Link></li>
           <li>
             <a to="#">Announcement</a>
             <ul>
-              <li><Link to={'/callForPaper'}>Call for Papers</Link></li>
-              <li><Link to={'/author-guidelines'}>Author Guidelines</Link></li>
-              <li><Link to={'/important-dates'}>Important Dates</Link></li>
+              <li onClick={handleToggle}><Link to={'/callForPaper'}>Call for Papers</Link></li>
+              <li onClick={handleToggle}><Link to={'/author-guidelines'}>Author Guidelines</Link></li>
+              <li onClick={handleToggle}><Link to={'/important-dates'}>Important Dates</Link></li>
             </ul>
           </li>
           <li>
             <a to="#">Organization</a>
             <ul>
-              <li><Link to={'/steering-commitee'}>Steering Committee</Link></li>
-              <li><Link to="Organization_Committee.html">Organizing Committee</Link></li>
-              <li><Link to={'/program-commitee'}>Program Committee</Link></li>
+              <li><Link onClick={handleToggle} to={'/steering-commitee'}>Steering Committee</Link></li>
+              <li><Link onClick={handleToggle} to="Organization_Committee.html">Organizing Committee</Link></li>
+              <li><Link onClick={handleToggle} to={'/program-commitee'}>Program Committee</Link></li>
             </ul>
           </li>
           <li>
             <a to="#">Program/Registration</a>
             <ul>
-              <li><a to="#">Agenda & Program</a></li>
-              <li><Link to={'/registration'}>Registration</Link></li>
-              <li><Link to="Registration_Policy.html">Registration Policy</Link></li>
-              <li><Link to="#">Hotel Reservation</Link></li>
-              <li><Link to={'/speakers'}>Keynotes & Invited Talks</Link></li>
-              <li><a to="#">Accepted Papers</a></li>
-              <li><a to="#">Tutorials</a></li>
+              {/* <li><a to="#">Agenda & Program</a></li> */}
+              <li><Link onClick={handleToggle} to={'/registration'}>Registration</Link></li>
+              {/* <li><Link to="Registration_Policy.html">Registration Policy</Link></li> */}
+              {/* <li><Link to="#">Hotel Reservation</Link></li> */}
+              <li><Link onClick={handleToggle} to={'/speakers'}>Keynotes & Invited Talks</Link></li>
+              {/* <li><a to="#">Accepted Papers</a></li> */}
+              {/* <li><a to="#">Tutorials</a></li> */}
             </ul>
           </li>
           <li>
             <a to="#">Workshops</a>
             <ul>
-              <li><a to="Call_for_percaa_workshop.html">PerCAA 2019 Workshops</a></li>
+              {/* <li><a to="Call_for_percaa_workshop.html">PerCAA 2019 Workshops</a></li>onClick={handleToggle}  */}
             </ul>
           </li>
           <li>
             <a to="#">Information</a>
             <ul>
-              <li><Link to={'/venue'}>Venue</Link></li>
-              <li><a to="How_to_reach.html">How to Reach</a></li>
-              <li><Link to={'/local-attractions'}>Local Attraction</Link></li>
+              <li><Link onClick={handleToggle} to={'/venue'}>Venue</Link></li>
+              {/* <li><a to="How_to_reach.html">How to Reach</a></li> */}
+              <li><Link onClick={handleToggle} to={'/local-attractions'}>Local Attraction</Link></li>
             </ul>
           </li>
-          <li><Link to={'/contact'}>Contact</Link></li>
+          <li><Link onClick={handleToggle} to={'/contact'}>Contact</Link></li>
         </ul>
       </nav>
     </>
