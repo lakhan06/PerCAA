eed310290084acab920f04915edba5e826e864c1
Chinmay Jain <chinmayluhadiya62@gmail.com>
2024-07-16 14:27:25 +0530

    top-bar-update

--- src/components/topComp/TopComp.jsx
+++ src/components/topComp/TopComp.jsx
@@ -2,13 +2,12 @@ import React from 'react'
 import './topComp.css'
 import pce from '/Assets/Poornima_Logo.png'
 import ACM from '/logos/acmChapterLogo.png'
-import MNIT from '/logos/Mnit_logo.png'
+import SP from '/logos/Springer_logo.png'
 import BL  from '/Assets/bentham_logo.png'
-import IEEE from '/logos/IEE_logo.jpg'
+// import IEEE from '/logos/IEE_logo.jpg'
 function TopComp() {
   return (
     <div className='topComp'>
-
     <marquee className="topmarquee" scrollamount="10">International Conference on Pervasive Computing Advances and Applications</marquee>
         <div className='top'>
             <div className="top-logos">
@@ -21,14 +20,14 @@ function TopComp() {
                 <div className='top-line'> -- Dates -- </div>
             </div>
             <div className="top-logos">
-            <img src={MNIT} alt="mnit_logo" className='mnit_logo'/>
+            <img src={SP} alt="mnit_logo" className='sp_logo'/>
             <img src={BL} alt='Bentham Logo'/>
             </div>
         </div>
         <div className="smallmarquee" scrollamount="10"> <div className="top-logos-marquee">
                 <img src={pce} alt="poornima university logo" className='pce_logo'/>
                 <img src={ACM} alt="acm chapter logo" className='acm_logo'/>
-                <img src={MNIT} alt="mnit_logo" className='mnit_logo'/>
+                <img src={SP} alt="mnit_logo" className='mnit_logo'/>
                 <img src={BL} alt='Bentham Logo'/>
             </div></div>
     </div>
