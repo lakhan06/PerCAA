cee0540d8c5b8f3bd425459bbe0de5183298564c
Aryan <aryanjainyk@gmail.com>
2024-07-31 11:16:51 +0530

    registration table edited

--- src/components/Registration/Registration.jsx
+++ src/components/Registration/Registration.jsx
@@ -3,6 +3,28 @@ import RightContent from "../RightContent/RightContent";
 import styles from "./Registration.module.css"
 import Footer from "../Footer/Footer";
 const Registration = () => {
+
+    const tableData = {
+        categories: [
+            "Regular author academician/industry person",
+            "Regular author academician/industry person (IEEE/ACM)",
+            "Regular author student (Ph.D./PG)",
+            "Regular author student (Ph.D./PG) IEEE/ACM",
+            "Attendee / Co-author",
+        ],
+        earlyBid: {
+            national: ["₹8100", "₹7200", "₹6200", "₹5400", "60% of Respective Category"],
+            international: ["$220", "$240", "$140", "$120", "60% of Respective Category"],
+        },
+        lateRegistration: {
+            national: ["₹9100", "₹8100", "₹7200", "₹6300", "60% of Respective Category"],
+            international: ["$330", "$300", "$240", "$150", "60% of Respective Category"],
+        },
+    };
+
+    const { categories, earlyBid, lateRegistration } = tableData;
+
+
     return (
         <>
             <div className="papaContainer">
@@ -11,6 +33,36 @@ const Registration = () => {
                         Registration Fees :
                     </div>
                     <div className="tableWrapper">
+                        <table border="1" cellspacing="0" cellpadding="2" width="99%">
+                            <thead>
+                                <tr>
+                                    <th width="20%" align="center"><b>Category</b></th>
+                                    <th colspan="2" width="40%" align="center"><b>Early Bid</b></th>
+                                    <th colspan="2" width="40%" align="center"><b>Late Registration</b></th>
+                                </tr>
+                                <tr>
+                                    <td width="20%" align="center">&nbsp;</td>
+                                    <td width="20%" align="center">National Authors (INR)</td>
+                                    <td width="20%" align="center">International Authors ($)</td>
+                                    <td width="20%" align="center">National Authors (INR)</td>
+                                    <td width="20%" align="center">International Authors ($)</td>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {categories.map((category, index) => (
+                                    <tr key={index}>
+                                        <td align="center">{category}</td>
+                                        <td align="center">{earlyBid.national[index]}</td>
+                                        <td align="center">{earlyBid.international[index]}</td>
+                                        <td align="center">{lateRegistration.national[index]}</td>
+                                        <td align="center">{lateRegistration.international[index]}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+
+                    </div>
+                    {/* <div className="tableWrapper">
                         <table border="1" cellspacing="0" cellpadding="2" width="99%">
                             <tr>
                                 <td width="6%" align="center"><b>Registration Category</b></td>
@@ -119,7 +171,7 @@ const Registration = () => {
                                 <td align="center">&nbsp; <b>N/A</b></td>
                             </tr>
                         </table>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="rightContainer">
                     <RightContent></RightContent>
