179f6be59bfce14feaaf7db47501adcb04c7aca7
Aryan <aryanjainyk@gmail.com>
2024-07-31 14:12:34 +0530

    ad

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,19 +9,17 @@ const Contact = () => {
             <div className="papaContainer">
                 <div className="leftContainer">
                     <div className="contact">
-                        <h1 className='contactSectionHeading'>Contact Us</h1>
+                        <h1 className='contactSectionHeading'>Contact Details for Correspondence / Query</h1>
                         <div className="contactCard">
-                            <div className="contactCardLeft">
-                                <img src="https://www.poornima.org/uploads/team/1716886760.jpg" alt="" />
-                                <div className="contactName">Dr. Nikita Jain</div>
-                                <div className="contactDesignation">Program Chair</div>
-                            </div>
-                            <div className="contactCardRight">
-                                {/* <p className='contact_CI1 contactborder'>Contact Information</p> */}
-                                <p className='contact_mailphone contactborder'><MdEmail /> <a href="mailto:nikita.jain@poornima.org">nikita.jain@poornima.org</a></p>
-                                <p className='contact_mailphone contactborder'><FaPhoneAlt /> <a href="tel:+919413069023">+91 9413069023</a></p>
-                                <p className='contactborder'><span>Address:</span> ISI-6, RIICO Institutional Area, Sitapura, Jaipur-302022</p>
-                                <p className='contactborder'><span>Conference Email:</span> <a href="mailto:conference@example.com">conference@example.com</a></p>
+                            <div className="contactCardDetails">
+                                <p><span>Convener: </span>PERCAA -2024</p>
+                                <p><span>E-mail: </span>percaa2024@poornima.org</p>
+                                <p>
+                                    <span>Contact Number(s): </span><br />
+                                    <a href="tel:+919897257571">+91-9897257571</a>  
+                                    <br className="break" />
+                                    <a href="tel:+917318158178">+91-7318158178</a>
+                                </p>
                             </div>
                         </div>
                     </div>
