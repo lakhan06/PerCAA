8aaf82891852ecc80caa7852d027aa4e99fb781e
Abhinav Arora <abhinavarora310@gmail.com>
2024-08-01 16:08:35 +0530

    init

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = ()=>{
             <div className="footer-heading">
                 <sup>© </sup>2024 Poornima College Of Engineering
             </div>
-            <a href="https://percaa.poornima.org/">www.poornima.org/PERCAA2024</a>
+            <a href="https://percaa.poornima.org/">percaa.poornima.org</a>
         </div>
     )
 }
