27c276cec81dc8b41094644d831bee5dbafaa0b7
Abhinav Arora <abhinavarora310@gmail.com>
2024-08-07 09:42:54 +0530

    init

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,19 +10,30 @@ const Contact = () => {
                 <div className="leftContainer">
                     <div className="contact">
                         <h1 className='contactSectionHeading'>Contact Details for Correspondence / Query</h1>
-                        <div className="contactCard">
-                            <div className="contactCardDetails">
-                                <p><span>Convener: </span>PERCAA -2024</p>
-                                <p><span>E-mail: </span>percaa2024@poornima.org</p>
-                                <p>
-                                    <span>Contact Number(s): </span><br />
-                                    <a href="tel:+919897257571">+91-9897257571</a>  
-                                    <br className="break" />
-                                    <a href="tel:+917318158178">+91-7318158178</a>
-                                </p>
+                        <div className="container_wrapper">
+                        <div className="container">
+                            <div className="card-contact">
+                                <h2>Dr. Nikita Jain</h2>
+                                <p style={{fontWeight:"bold" , fontSize:".8rem"}}>Organizing Chair</p>
+                                <p style={{fontWeight:"bold" , fontSize:".8rem" , marginTop:"10px"}}>Contact : +91 9413069023</p>
+                                <p style={{fontWeight:"bold" , fontSize:".7rem" , marginTop:"10px"}}>Email : percaa2024@poornima.org</p>
+                            </div>
+                            <div className="card-contact">
+                                <h2>Dr. Abhishek Sharma</h2>
+                                <p style={{fontWeight:"bold" , fontSize:".8rem"}}>Convenor</p>
+                                <p style={{fontWeight:"bold" , fontSize:".8rem" , marginTop:"10px"}}>Contact : +91 9897257571</p>
+                                <p style={{fontWeight:"bold" , fontSize:".7rem" , marginTop:"10px"}}>Email : percaa2024@poornima.org</p>
+                            </div>
+                            <div className="card-contact">
+                                <h2>Ms. Sonam Gaur</h2>
+                                <p style={{fontWeight:"bold" , fontSize:".8rem"}}>Co Convenor</p>
+                                <p style={{fontWeight:"bold" , fontSize:".8rem" , marginTop:"10px"}}>Contact : +91 9509885411</p>
+                                <p style={{fontWeight:"bold" , fontSize:".7rem" , marginTop:"10px"}}>Email : percaa2024@poornima.org</p>
+
                             </div>
                         </div>
-                    </div>
+                        </div>
+                        </div>
                 </div>
                 <div className="rightContainer">
                     <RightContent />
