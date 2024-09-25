e589692ddfa2e0b94ece1bd10cce051e5f67ba24
Abhinav Arora <abhinavarora310@gmail.com>
2024-09-25 12:12:08 +0530

    init

--- src/components/ImportantDates/ImportantDates.jsx
+++ src/components/ImportantDates/ImportantDates.jsx
@@ -4,9 +4,9 @@ import Footer from "../Footer/Footer";
 const ImportantDates = () => {
     const dates = [
         { event: "Last Date of Paper Submission", date: ['August 20, 2024', 'September 30, 2024'] },
-        { event: "Early Bird Registration" , date:['October 05, 2024']},
-        { event: "Acceptance Notification", date: ['August 31, 2024', 'October 08, 2024'] },
-        { event: "Camera Ready Paper Submission", date: ['September 30, 2024', 'October 10, 2024'] },
+        { event: "Acceptance Notification", date: ['August 31, 2024', 'October 05, 2024'] },
+        { event: "Camera Ready Paper Submission", date: ['September 30, 2024', 'October 08, 2024'] },
+        { event: "Early Bird Registration" , date:['October 10, 2024']},
         { event: "Last Date of Registration", date: ['October 10, 2024', 'October 12, 2024'] },
         { event: "Conference Date", date: ['October 18-19, 2024'] },
     ];
